3dcdd412a7e2bae2b9a9314d3fbcc39d11bf3201
Cameron Smith <cameron.ray.smith@gmail.com>
2022-10-16 17:33:29 -0400

    handle requests exceptions
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/test_hmp/wikipedia.py
+++ src/test_hmp/wikipedia.py
@@ -2,6 +2,7 @@
 
 See `API documentation <https://en.wikipedia.org/api/rest_v1/#/>`_.
 """
+import click
 import requests
 
 
@@ -10,7 +11,11 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 def random_page():
     """Return a random page."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
-    return data
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            data = response.json()
+        return data
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message) from error
