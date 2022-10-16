63b8458f0ce427380ccac1995a9d2b433722ba60
Cameron Smith <cameron.ray.smith@gmail.com>
2022-10-16 19:48:20 -0400

    add option to set wikipedia language
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- src/test_hmp/__main__.py
+++ src/test_hmp/__main__.py
@@ -9,10 +9,18 @@ from . import wikipedia
 
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main() -> None:
+def main(language) -> None:
     """Test Hypermodern Python."""
-    data = wikipedia.random_page()
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
