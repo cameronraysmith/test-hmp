76eb75e2a9a4f1af9e37a102e6af8d1cf452b3a2
Cameron Smith <cameron.ray.smith@gmail.com>
2022-10-16 12:34:22 -0400

    mock requests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,9 @@
 """Test cases for the __main__ module."""
+from unittest.mock import Mock
+
 import pytest
 from click.testing import CliRunner
+from pytest_mock import MockFixture
 
 from test_hmp import __main__
 
@@ -11,7 +14,18 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
+@pytest.fixture
+def mock_requests_get(mocker: MockFixture) -> Mock:
+    """Fixture for mocking requests.get."""
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+
+def test_main_succeeds(runner: CliRunner, mock_requests_get: Mock) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
